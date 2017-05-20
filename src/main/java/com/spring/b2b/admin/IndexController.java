1ac74d94efd01ed4600dc417b2b9a2eaa8564c85
quanchunlindellpc <quanchunlindellpc@163.com>
2017-05-20 17:49:53 +0800

    business to business marketing

---
+++ src/main/java/com/spring/b2b/admin/IndexController.java
@@ -0,0 +1,38 @@
+package com.spring.b2b.admin;
+
+//import java.text.DateFormat;
+//import java.util.Date;
+//import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class IndexController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+	@RequestMapping(value = "/admin/index.do", method = RequestMethod.GET)
+	public String index() {
+		//logger.info("Welcome home! The client locale is {}.", locale);
+		
+		/*Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";*/
+		logger.info("MSG: ");
+		return "admin/index";
+	}
+	
+}
